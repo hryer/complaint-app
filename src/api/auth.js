3c88df2c61ec9c32dbda470ebfc0bce9601b5a44
Hryer <harryermawan@gmail.com>
2019-07-04 16:06:38 +0700

    chore(auth): delete comment

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,15 +1,6 @@
 import axios from 'axios'
 import {urlLogin} from '../config';
 
-/* Result request Json (response)
-{
-    "success": true,
-    "data": {
-        "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MywibmFtZSI6IkRyLiBSb3NzIEdyZWVuZmVsZGVyIEREUyIsImNvbXBhbnlfbmFtZSI6ImFoYSBjb3JwIiwiZW1haWwiOiJhZG1pbkBlZmlzaGVyeS5jb20iLCJwaG9uZSI6IjYyODUyMjQ3NTIxODAiLCJhZGRyZXNzIjoiQ2lrdXRyYSBCYXJ1IFYiLCJwaWN0dXJlIjpudWxsLCJjcmVhdGVkX2F0IjpudWxsLCJ1cGRhdGVkX2F0IjpudWxsLCJhcHBfdG9rZW4iOiJpbml0b2tlbnRlc3QiLCJjdXN0X2lkIjoiY3VzdF8wMCIsImxvY2F0aW9uIjoiMDIyIiwiaXNBZG1pbiI6MSwiaW50ZXJlc3QiOm51bGwsInN0YXR1cyI6ImFjdGl2ZSIsImdkcml2ZV9mb2xkZXJfaWQiOiIiLCJpc19uYmlvdCI6MCwic3ViIjozLCJpc3MiOiJodHRwOi8vc3RhZ2luZy5kYXNoLWFwaS5lZmlzaGVyeS5jb20vbG9naW4iLCJpYXQiOjE1NjIwNTI3ODcsImV4cCI6MTU2MjEzOTE4NywibmJmIjoxNTYyMDUyNzg3LCJqdGkiOiJROU5Tbzg3VllRcDZuVVd3In0.g4nB96ITCDoD078YNSbXGOIx38bXJB5q8dRAgPZUdPs"
-    }
-}
-*/
-
 export const requestLogin = (payload) => {
   var config = {
     headers: {
